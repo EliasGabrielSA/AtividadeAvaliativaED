4a00c372cf7045280ea5979e49b8c6c2bc133d42
elias <eg.souzaandrade@gmail.com>
2023-05-28 21:14:39 -0300

    Correção de bugs necessaria

--- main.c
+++ main.c
@@ -2,13 +2,45 @@
 	Exercício Avaliativo 2
 	Disciplina de Estruturas de Dados
 	
-	Aluno1: Nome do aluno 1
-	Aluno2: Nome do aluno 2
+	Aluno1: Elias Gabriel de Souza Andrade
+	Aluno2: Pedro Poubel
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include "nutri.h"
 
 int main(int argc, char* argv[]) {
-    return 0;
+
+	FILE *ptrfile;
+	char path[100];
+	int numPac;
+
+	Paciente vetPac[numPac];
+	Paciente *vetPacPointer = vetPac;
+	//Paciente *vetPointer = vetPac;
+
+	scanf("%s", path);
+	scanf("%d", &numPac);
+
+	ptrfile = fopen(path, "r");
+
+	if (ptrfile == NULL) {
+		printf("Arquivo nao encontrado.\n");
+	} else if (numPac < 1) {
+		printf("Informe um valor maior que 0.\n");
+	} else if (numPac > 100) {
+		printf("O valor informado deve estar no intervalo: 0 < valor <= 100\n");
+	} else {
+		lerArquivoPacientes(ptrfile, vetPac, numPac);
+
+		for(int i = 0; i < numPac; i++) {
+			vetPac[i].IMC = calcularIMC(&vetPacPointer[i]);
+		}
+
+		for(int i = 0; i < numPac; i++) {
+			mostrarDadosPaciente(vetPac[i]);
+		}
+	}
+
+	fclose(ptrfile);
 }
--- nutri.c
+++ nutri.c
@@ -1,1 +1,35 @@
-// Todas as funções definidas no arquivo "nutri.h" deverão ser implementas neste arquivo
+struct _nutriPac {
+	char nome[30];
+	double altura;
+	double peso;
+	double IMC;
+};
+typedef struct _nutriPac Paciente;
+
+void lerArquivoPacientes(FILE *ptrFile, Paciente vetPac[], int numPac) {
+    
+    for(int i = 0; i < numPac; i++) {
+        fscanf(ptrFile, "%[^,], %u, %u", vetPac[i].nome, vetPac[i].altura, vetPac[i].peso);
+    }
+}
+
+void calcularIMC(Paciente *pac){
+	return pac->peso / (pac->altura * pac->altura);
+}
+
+void mostrarDadosPaciente(Paciente pac) {
+	char classific_pac[16];
+	if(pac.IMC < 18.5) {
+		classific_pac[] = "Baixo peso";
+	} else if (pac.IMC >= 18.5 && pac.IMC <= 24.9) {
+		classific_pac[] = "Peso normal";
+	} else if (pac.IMC >= 25.0 && pac.IMC <= 29.9) {
+		classific_pac[] = "Sobrepeso";
+	} else if (pac.IMC >= 30.0 && pac.IMC <= 39.9) {
+		classific_pac[] = "Obesidade";
+	} else if (pac.IMC >= 40) {
+		classific_pac[] = "Obesidade grave";
+	}
+
+	printf("Nome  :   %s\nPeso  :	%u\nAltura: %u\nIMC   :		%u\n\nClassificacao: %s\n", pac.nome, pac.peso, pac.altura, pac.IMC, classific_pac);
+}
